dfd3d612b7d9247dc1e64ae6b1d69305d07758ac
Sarabdeep Singh Bilkhu <sanjhbilkhu2006@gmail.com>
2025-11-28 16:20:58 +0530

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/',
+  base: '/SarabdeepSBilkhu.github.io/',
   plugins: [react()],
 })
